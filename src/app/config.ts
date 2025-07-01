c699de5113eca529f397093201c3b0e5b04c8208
lumoswiz <lumoswiz@protonmail.com>
2025-07-01 13:20:14 +0200

    refactor: rename ACCOUNT_ID to BITTE_API_KEY for compatibility

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,13 +1,15 @@
-import { DEPLOYMENT_URL } from "vercel-url";
+import { DEPLOYMENT_URL } from 'vercel-url';
 
-const ACCOUNT_ID = process.env.ACCOUNT_ID;
+const ACCOUNT_ID = process.env.BITTE_API_KEY;
 
 // Set the plugin url in order of BITTE_CONFIG, env, DEPLOYMENT_URL (used for Vercel deployments)
-const PLUGIN_URL = DEPLOYMENT_URL || `${process.env.NEXT_PUBLIC_HOST || 'localhost'}:${process.env.PORT || 3000}`;
+const PLUGIN_URL =
+  DEPLOYMENT_URL ||
+  `${process.env.NEXT_PUBLIC_HOST || 'localhost'}:${process.env.PORT || 3000}`;
 
 if (!PLUGIN_URL) {
   console.error(
-    "!!! Plugin URL not found in env, BITTE_CONFIG or DEPLOYMENT_URL !!!"
+    '!!! Plugin URL not found in env, BITTE_CONFIG or DEPLOYMENT_URL !!!'
   );
   process.exit(1);
 }
