5efc0610558d6c19fa867cf7dcd019da0fb283c9
lumoswiz <lumoswiz@protonmail.com>
2025-07-03 16:05:14 +0200

    chore: update addresses

--- src/app/api/tools/addresses.ts
+++ src/app/api/tools/addresses.ts
@@ -5,35 +5,35 @@ const DUMMY: Address = '0x9EE7173aE1116E3150d3B9dFd427C12cb777FB5d';
 
 export const BITTE_TOKEN: Record<number, Address> = {
   [base.id]: DUMMY,
-  [baseSepolia.id]: '0xE67920Dc41ecCac023850036434E1122c1E8D2A5',
+  [baseSepolia.id]: '0x93e67DA623385eb6f15b5cdf67DC9E82b1F154E2',
 };
 
 export const AGENT_STAKING: Record<number, Address> = {
   [base.id]: DUMMY,
-  [baseSepolia.id]: '0x80BC777B7aAd56736cBC36B99989cD6dc864c533',
+  [baseSepolia.id]: '0x72bbc3888d5E3cEc1FF41eA67E095623C383f2E5',
 };
 
 export const DELEGATE_STAKING: Record<number, Address> = {
   [base.id]: DUMMY,
-  [baseSepolia.id]: '0xbF6D376A0a99D8Dc9C8A9617058cc72dBb465cC7',
+  [baseSepolia.id]: '0x80Bc786e46084a31e9015bcd2b913d2f06E39b57',
 };
 
 export const REWARDS_DISTRIBUTOR: Record<number, Address> = {
   [base.id]: DUMMY,
-  [baseSepolia.id]: '0x5B41927251945584785aD1D036D060b5E47B6a16',
+  [baseSepolia.id]: '0x8868500c66999d06008de5097B49960570A8B9A0',
 };
 
 export const WETH: Record<number, Address> = {
   [base.id]: '0x4200000000000000000000000000000000000006',
-  [baseSepolia.id]: '0x1F77ca71Ec6477a3E659536C0047002eF44DBFd4',
+  [baseSepolia.id]: '0xf1213d8E59a56E740dEc35fcEeB14a9287285bde',
 };
 
 export const VIRTUAL_STAKING: Record<number, Address> = {
   [base.id]: DUMMY,
-  [baseSepolia.id]: '0x915142E78E78A704233E0b9b20CB0035463E496f',
+  [baseSepolia.id]: '0x8574FB95c76eB56929d6B36E378495dC0C522A66',
 };
 
 export const BITTE_VIRTUAL_TOKEN: Record<number, Address> = {
   [base.id]: DUMMY,
-  [baseSepolia.id]: '0xEC0807c715dCB7340c236Ae82976B8fF2A81De2f',
+  [baseSepolia.id]: '0x665E63dD9558cd66414bc0205172B9B32Ba99f0b',
 };
